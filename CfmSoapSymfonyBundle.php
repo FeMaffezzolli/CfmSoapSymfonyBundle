09b816edb411543874d5b213e802995b61195401
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-10-28 17:12:47 -0300

    fixed import statement

--- CfmSoapSymfonyBundle.php
+++ CfmSoapSymfonyBundle.php
@@ -3,12 +3,12 @@
 namespace Cfm\SoapSymfonyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Cfm\SoapSymfonyBundle\DependencyInjection\UnconventionalExtensionClass;
+use Cfm\SoapSymfonyBundle\DependencyInjection\CfmSoapSymfonyExtension;
 
 class CfmSoapSymfonyBundle extends Bundle
 {
     public function getContainerExtension()
     {
-        return new UnconventionalExtensionClass();
+        return new CfmSoapSymfonyExtension();
     }
 }
